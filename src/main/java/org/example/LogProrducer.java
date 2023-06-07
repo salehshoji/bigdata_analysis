9643e8f74d70b317d5b8bb0ff87b5338eeb5f40d
saleh <mohamadsaleh.shojai@yahoo.comm>
2023-06-07 11:00:55 +0330

    remove redundant comments

--- src/main/java/org/example/LogProrducer.java
+++ src/main/java/org/example/LogProrducer.java
@@ -38,30 +38,6 @@ public class LogProrducer {
             }
         }
 
-//        String[] users = {"eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther"};
-//        String[] items = {"book", "alarm clock", "t-shirts", "gift card", "batteries"};
-//        try (final Producer<String, String> producer = new KafkaProducer<>(props)) {
-//            final Random rnd = new Random();
-//            final Long numMessages = 10L;
-//
-//            for (Long i = 0L; i < numMessages; i++) {
-//                String user = users[rnd.nextInt(users.length)];
-//                String item = items[rnd.nextInt(items.length)];
-//
-//                producer.send(
-//                        new ProducerRecord<>(topic, user, item),
-//                        (event, ex) -> {
-//                            if (ex != null)
-//                                ex.printStackTrace();
-//                            else
-//                                System.out.printf("Produced event to topic %s: key = %-10s value = %s%n", topic, user, item);
-//                        });
-//            }
-//            System.out.printf("%s events were produced to topic %s%n", numMessages, topic);
-//        }
-
-
-
     }
 
     private static String readfile(File log) throws FileNotFoundException {
