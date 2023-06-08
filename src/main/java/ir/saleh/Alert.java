7575315ebb01fa014d2269f2b7905e9e4f3b7e41
saleh <mohamadsaleh.shojai@yahoo.comm>
2023-06-08 22:15:42 +0330

    connect Alert with database

--- src/main/java/ir/saleh/Alert.java
+++ src/main/java/ir/saleh/Alert.java
@@ -1,6 +1,7 @@
 package ir.saleh;
 
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,5 +16,27 @@ public class Alert {
         this.alertName = alertName;
         this.description = description;
         alertsList.add(this);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DB();
+    }
+    public static void DB() throws SQLException {
+//        String selectDB = "USE database_saleh";
+        String sqlSelectAllPersons = "SELECT * FROM person";
+        String connectionUrl = "jdbc:mysql://localhost:3306/database_saleh";
+        Connection conn = DriverManager.getConnection(connectionUrl);
+//        conn.setCatalog("database_saleh");
+//        PreparedStatement ps = conn.prepareStatement(selectDB);
+//        ResultSet rs = ps.executeQuery();
+        PreparedStatement ps = conn.prepareStatement(sqlSelectAllPersons);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            long id = rs.getLong("ID");
+            String name = rs.getString("FIRST_NAME");
+            String lastName = rs.getString("LAST_NAME");
+            System.out.println(id + "||" + name + "||" + lastName);
+            // do something with the extracted data...
+        }
     }
 }
